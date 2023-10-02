0f39168a31964e39062f36c251f911cfbed60549
Barry Khan <37813971+BarryUX@users.noreply.github.com>
2023-10-02 16:55:44 +0100

    Setup and landing pages, plus further work on hist

--- app/routes.js
+++ app/routes.js
@@ -6,14 +6,25 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
+
 // Add your routes here
-router.post('/add-format-answer', function(request, response) {
+router.post('/1-0-0/setup-answer', function(request, response) {
 
     var formatfile = request.session.data['format-file']
     if (formatfile == "yes"){
-        response.redirect("/upload-file")
+        response.redirect("/1-0-0/upload-file")
     } else {
-        response.redirect("/add-reason")
+        response.redirect("/1-0-0/add-reason")
+    }
+})
+
+router.post('/1-0-0/add-format-answer', function(request, response) {
+
+    var formatfile = request.session.data['format-file']
+    if (formatfile == "yes"){
+        response.redirect("/1-0-0/upload-file")
+    } else {
+        response.redirect("/1-0-0/add-reason")
     }
 })
 
