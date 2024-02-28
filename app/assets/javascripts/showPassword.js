bd10970297069e2595bb6e6e6948032a3b55a134
Elliot Butler <157047814+elliot-butler-gds@users.noreply.github.com>
2024-02-28 17:07:19 +0000

    Draft password reset screens

--- app/assets/javascripts/showPassword.js
+++ app/assets/javascripts/showPassword.js
@@ -3,32 +3,6 @@
 // https://prototype-kit.service.gov.uk/docs/adding-css-javascript-and-images
 //
 
-window.GOVUKPrototypeKit.documentReady(() => {
-  // Add JavaScript here
-})
-
-new MOJFrontend.FilterToggleButton({
-  bigModeMediaQuery: "(min-width: 48.063em)",
-  startHidden: true,
-  toggleButton: {
-    container: $(".moj-action-bar__filter"),
-    showText: "Show filter",
-    hideText: "Hide filter",
-    classes: "govuk-button--secondary",
-  },
-  closeButton: {
-    container: $(".moj-filter__header-action"),
-    text: "Close",
-  },
-  filter: {
-    container: $(".moj-filter"),
-  },
-});
-
-
-
-
-
 // Define the ShowPassword module
 window.GOVUK = window.GOVUK || {},
 window.GOVUK.Modules = window.GOVUK.Modules || {},
